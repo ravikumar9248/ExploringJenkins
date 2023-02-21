ab9f03bb4aeb77e13f36a1934f9fbea92cc8bb98
RaviKumar Mayakuntla <RaviKumar.Mayakuntla@FISGLOBAL.COM>
2023-02-21 20:44:39 +0530

    Add project files.

---
+++ ExploringJenkins/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ExploringJenkins
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
